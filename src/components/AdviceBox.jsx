f4850ecaa844f6d3556ef9aaf1bc27370fdff99d
queen-favour <elechifavour3@gmail.com>
2024-08-26 11:23:19 +0100

    fix: mobile view

--- src/components/AdviceBox.jsx
+++ src/components/AdviceBox.jsx
@@ -21,16 +21,16 @@ const AdviceBox = () => {
 
   return (
     <div className="bg-custom-dark-blue h-screen flex items-center justify-center ">
-      <div className="bg-dark-grayish-blue relative flex flex-col items-center justify-center p-4 rounded-lg w-full h-auto gap-4 lg:p-9 lg:rounded-xl lg:w-[25rem] lg:h-[20rem] lg:mx-0 mx-6">
-        <p className="text-neon-green uppercase text-sm tracking-wide">
+      <div className="bg-dark-grayish-blue relative flex flex-col items-center justify-center p-4 rounded-lg w-full h-auto gap-4 align-middle lg:p-9 lg:rounded-xl lg:w-[25rem] lg:h-[20rem] lg:mx-0 mx-6">
+        <p className="text-neon-green uppercase text-sm tracking-wide font-[600]">
           Advice #<span>{slip.id}</span>
         </p>
         <p className="text-[28px] font-[600] text-light-cyan">
           "{slip.advice}"
         </p>
-        <img className="" src={divider} alt="" />
+        <img className="pb-10" src={divider} alt="" />
         <div
-          className="bg-neon-green h-fit p-[0.8rem] rounded-full absolute -bottom-5 active:shadow-[0_0_15px_4px_rgba(43,252,140,1)] hover:shadow-[0_0_20px_4px_rgba(43,252,140,0.75)] drop-shadow-xl transition-shadow duration-300"
+          className="cursor-pointer bg-neon-green h-fit p-[0.8rem] rounded-full absolute -bottom-5 active:shadow-[0_0_15px_4px_rgba(43,252,140,1)] hover:shadow-[0_0_20px_4px_rgba(43,252,140,0.75)] drop-shadow-xl transition-shadow duration-300"
           onClick={() => fetchQuote()}
         >
           <img src={dice} alt="" />
